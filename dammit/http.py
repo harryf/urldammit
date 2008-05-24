1005919173966c6fd6739d1f63a157dc5d184322
Harry Fuecks <hfuecks@gmail.com>
2008-05-24 21:48:13 +0200

    add lru caching, get status codes working

---
+++ dammit/http.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+status = {
+    200: '200 OK',
+    201: '201 Created',
+    202: '202 Accepted',
+    300: '300 Multiple Choices',
+    301: '301 Moved Permanently',
+    302: '302 Found',
+    303: '303 See Other',
+    304: '304 Not Modified',
+    305: '305 Use Proxy',
+    306: '305 Switch Proxy',
+    307: '307 Temporary Redirect',
+    404: '404 Not Found',
+    }
